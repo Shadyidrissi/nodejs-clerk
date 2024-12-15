fe148142a95a2238cf5d2525e390873fbf09d16f
Shady Idrissi <chadiidac@gmail.com>
2024-12-15 15:27:18 +0100

    starting project

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import {clerkClient} from '@clerk/express'
+
+const app = express()
+const port = 4000
+
+dotenv.config();
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async(req, res) => {
+    const {data} =await clerkClient.users.getUserList()
+    console.log(data)
+    res.send(data)
+  })
+  
+  app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
